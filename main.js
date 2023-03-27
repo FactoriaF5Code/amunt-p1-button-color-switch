a4af6051a2fdb01ffcd4774fec3850b242e73fab
Javi <javi@jsrois.net>
2023-03-27 11:26:03 +0200

    Añadido ejemplo

---
+++ main.js
@@ -0,0 +1,9 @@
+// cuando pulse en el primer botón,
+let button = document.getElementById("red")
+button.addEventListener("click", function() {
+    // el recuadro se pone de color rojo
+    
+});
+
+
+
